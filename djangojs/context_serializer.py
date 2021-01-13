628f89758d5cf8b988d3654056211350a4bc19b2
Robert-Chiang <robert@streetvoice.com>
2021-01-13 14:49:46 +0800

    Fix AttributeError: 'MIDDLEWARE_CLASSES' setting not found

--- djangojs/context_serializer.py
+++ djangojs/context_serializer.py
@@ -105,7 +105,7 @@ class ContextSerializer(object):
             'is_superuser': False,
             'permissions': tuple(),
         }
-        if 'django.contrib.sessions.middleware.SessionMiddleware' in settings.MIDDLEWARE_CLASSES:
+        if 'django.contrib.sessions.middleware.SessionMiddleware' in settings.MIDDLEWARE:
             user = self.request.user
             data['user']['is_authenticated'] = user.is_authenticated()
             data['user']['id'] = user.id
