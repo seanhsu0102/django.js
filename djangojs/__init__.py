0c52b384233b3295cbdff6c8988096df85785135
artin <cha798215@gmail.com>
2023-03-10 17:08:10 +0800

    Bump version from to 2.3.0

--- djangojs/__init__.py
+++ djangojs/__init__.py
@@ -3,7 +3,7 @@
 Django.js provide better integration of javascript into Django.
 '''
 
-__version__ = '2.0.1'
+__version__ = '2.3.0'
 __description__ = "Django JS Tools"
 
 #: Packaged jQuery version
